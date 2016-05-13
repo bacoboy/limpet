1333c72f2bd4bba26f7e3682a99a573869383344
Steve Hoffman <steve.hoffman@raise.com>
2016-05-12 20:28:22 -0500

    fix package script

--- package.sh
+++ package.sh
@@ -6,7 +6,6 @@
 #    exit 1
 #fi
 
-if [ -z 
 VERSION=`grep '^version' Cargo.toml | cut -d '"' -f 2`
 echo "Packaging version ${VERSION}..."
 cargo clean
@@ -14,4 +13,3 @@ docker run --rm -v `pwd`:/source -ti schickling/rust cargo build --release
 zip -j limpet_${VERSION}_linux_amd64.zip  target/release/limpet
 
 #curl -T limpet_${VERSION}_linux_amd64.zip -ubacoboy:${BINTRAY_API_KEY} -H "X-Bintray-Package:limpet" -H "X-Bintray-Version:${VERSION}" https://api.bintray.com/content/bacoboy/limpet/limpet
-
